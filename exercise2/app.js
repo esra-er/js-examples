215782010622926966c7bff9386bed3a6df3d430
esra-er <er.esragul@gmail.com>
2022-11-05 14:50:28 +0100

    exercise2

---
+++ exercise2/app.js
@@ -0,0 +1,7 @@
+let weight = 146;
+let height= 70;
+let katsayi1=0.45359237; // sabitler buyuk harflerle yazilir
+let katsayi2= 0.0254;
+
+let bmi= (weight*katsayi1)/(Math.pow(height*katsayi2,2))
+console.log(bmi);
