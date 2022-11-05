7d930a07aca658b742defd83f405019b6cf25d5d
esra-er <er.esragul@gmail.com>
2022-11-05 14:52:30 +0100

    exercise7

---
+++ exercise7/app.js
@@ -0,0 +1,30 @@
+// cozum1
+
+// let number = "232-23-5477";
+// const arr= number.split(`-`)
+// if (arr[0]>=100 && arr[0]<1000 && arr[1]<100 &&arr[1]>=10 && arr[2]>=1000 && arr[2]<10000) {
+//     console.log(` ${arr.join(`-`)} is a valid`)
+// }
+// else{
+//     console.log(`invalid date`);
+// }
+
+
+// cozum2
+
+// const mySsn = "745-45-60819";
+// let split =mySsn.split("-")
+
+// for (let index = 0; index < split.length; index++) {
+//     if (isNaN(split[index]) ) {
+//         console.log("invalid")
+//         break;
+//     }
+//     if (split[0].length===3 && split[1].length===2 && split[2].length===4) {
+//         console.log("valid")
+//         break;
+//     }else{
+//         console.log("invalid")
+//         break;
+//     }
+// }
