b1378caa45281913409d3f9f2846ff604988781e
esra-er <er.esragul@gmail.com>
2022-11-05 14:50:53 +0100

    exercise3

---
+++ exercise3/app.js
@@ -0,0 +1,15 @@
+let word= "process"
+// dizi seklinde böyle cozum
+
+// let arr= word.split("")
+
+// console.log(arr.length)
+// console.log(arr)
+// console.log(arr[0])
+// console.log(arr[arr.length-1])
+
+// string olarak bu sekilde cozum
+
+//  console.log(word.length)
+//  console.log(word.charAt())
+//  console.log(word[word.length-1])
