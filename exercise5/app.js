c02ff9adc9e20f9ddc2f7e031bd1fca06f5432bd
esra-er <er.esragul@gmail.com>
2022-11-05 14:51:44 +0100

    exercise5

---
+++ exercise5/app.js
@@ -0,0 +1,17 @@
+// let number = "125";
+// const arr= number.split("")
+
+// if (arr[0]==arr[2]) {
+//     console.log("true")
+// }else{
+//     console.log("false")
+// }
+
+let word="kazaaaaaaaaaaaaaaaaak"
+let word2=word.split('').reverse().join('');
+
+if (word===word2) {
+    console.log("true")
+} else {
+    console.log("false")
+}
